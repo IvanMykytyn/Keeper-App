0db1aa57c92736907a401c8efaa417a07e23b3b5
VanekCheck <mykytynivan04@gmail.com>
2022-03-13 15:35:49 +0200

    Upgrade Note Componentto more flexible one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import notes from "../notes";
 
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
 
-const App = () => {
-  return (
-    <div>
-      <Heading />
-      <Note />
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Heading />
+    {notes.map((note) => (
+      <Note key={note.key} title={note.title} content={note.content} />
+    ))}
+    <Footer />
+  </div>
+);
 
 export default App;
