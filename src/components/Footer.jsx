7199306086ddd46506d1ec51edd07bbecaa423a4
VanekCheck <mykytynivan04@gmail.com>
2022-03-13 13:51:54 +0200

    Starting Keeper App and add heading + one card, All with React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {currentYear}</p>
+    </footer>
+  );
+};
+
+export default Footer;
